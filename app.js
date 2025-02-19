2785b3c29c72ee623461dc3c2bbafa8a683ee69a
souvik mondal <38136428+KALYANIHF@users.noreply.github.com>
2025-02-20 03:12:22 +0530

    create course module is not working at this point need to check this tomorrow

--- app.js
+++ app.js
@@ -12,7 +12,7 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
 app.use("/api/v1/bootcamps", router);
-// app.use("/api/v1/courses", course);
+app.use("/api/v1/courses", course);
 app.use(errorHandler);
 
 app.listen(PORT, () => {
